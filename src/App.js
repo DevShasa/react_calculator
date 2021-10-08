ef32bda1986604dd5e22735ce3334c55fb95a463
Shasa <shasa.thuo@gmail.com>
2021-10-08 10:14:16 +0300

    displays

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Wrapper from "./components/Wrapper/Wrapper";
+
+export default class App extends Component{
+    render(){
+        return(
+            <div>
+                <Wrapper />
+            </div>
+        )
+    }
+}
---
+++ src/components/ButtonBox/Buttonbox.js
@@ -0,0 +1,5 @@
+import "./Buttonbox.css";
+const Buttonbox = ({children}) =>{
+    return <div className="buttonbox">{children}</div>
+};
+export default Buttonbox
