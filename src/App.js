040b2499ef7132996e858cb1dadd5944f14aff6c
Shasa <shasa.thuo@gmail.com>
2021-10-08 17:11:34 +0300

    functions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Wrapper from "./components/Wrapper/Wrapper";
+import Screen from "./components/Screen/Screen";
+import Buttonbox from "./components/ButtonBox/Buttonbox";
+import Button from "./components/Button/Button";
 
-export default class App extends Component{
-    render(){
-        return(
-            <div>
-                <Wrapper />
-            </div>
-        )
-    }
+const btnValues = [
+    ["C", "+-", "%", "/"],
+    [7, 8, 9, "X"],
+    [4, 5, 6, "-"],
+    [1, 2, 3, "+"],
+    [0, ",", "="],
+]
+
+export default function App(){
+
+    let [calc , setCalc] = useState({
+        sign: "",
+        num: 0,
+        res: 0,
+    })
+
+    return(
+        <Wrapper>
+            <Screen value={calc.num ? calc.num : calc.res} />
+            <Buttonbox>
+                {
+                    btnValues.flat().map((btn, i)=>
+                        <Button 
+                            key={i}
+                            className={btn === "=" ? "equals" : ""}
+                            value = {btn}
+                            onClick={
+                                btn === "C" ? resetClickHandler
+                                : btn === "+-" ? invertClickHandler
+                                : btn === "%"  ? percentClickHandler
+                                : btn === "=" ? equalsClickHandler
+                                : btn === "/" || btn === "X" || btn === "-" || btn === "+" ? signClickHandler
+                                : btn === "," ? commaClickHandler
+                                : numClickHandler
+                            }
+                        />    
+                    )
+                }
+            </Buttonbox>
+        </Wrapper>
+    )
 }
